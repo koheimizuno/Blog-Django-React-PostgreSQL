99bd9ada3a5d13ab0b25368f3138b2a15769e2b7
michioyamada <skygentleman626@gmail.com>
2023-09-20 16:56:39 +0400

    Implemented serializers.py file and  API endpoints Implement API endpoints using Django REST framework to handle CRUD operations for blog posts, user authentication, and other features.

--- blog_platform/blog/urls.py
+++ blog_platform/blog/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'posts', views.PostViewSet)
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'tags', views.TagViewSet)
+router.register(r'comments', views.CommentViewSet)
+
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', include(router.urls)),
+    path('postlist', views.PostListCreateView.as_view(), name='postlist')
 ]
--- blog_platform/blog/views.py
+++ blog_platform/blog/views.py
@@ -1,6 +1,59 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from .models import Post, Category, Tag, Comment
+from .serializers import PostSerializer, CategorySerializer, TagSerializer, CommentSerializer
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+
+class TagListCrateView(generics.ListCreateAPIView):
+    queryset = Tag.objects.all()
+    serializer = TagSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer = TagSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
 
 def home(request):
     return JsonResponse({'message': 'Testing App'})
